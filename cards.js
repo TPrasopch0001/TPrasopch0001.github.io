69f3840f022cc633067317f284341449a8459fdf
TPRASOPCH0001 <tprasopchaichan@hawk.iit.edu>
2023-01-17 20:40:32 -0600

    fixed list

--- cards.js
+++ cards.js
@@ -2,19 +2,19 @@
 
 let cards = new Array();
 
-cards[0] = './Cards/archangels.JPG';
+cards[0] = './Cards/archangels.png';
 cards[1] = './Cards/AvgRed.png';
 cards[2] = './Cards/Banshees.png';
 cards[3] = './Cards/ForbiddenForest.png';
 cards[4] = './Cards/JaqkRose.png';
-cards[5] = './Cards/jetsetters.jpg';
+cards[5] = './Cards/jetsetters.png';
 cards[6] = './Cards/Mando.png';
 cards[7] = './Cards/MartinWhite.png';
 cards[8] = './Cards/MonarchsBlue.png';
 cards[9] = './Cards/MoriBlack.png';
 cards[10] = './Cards/MoriWhite.png';
 cards[11] = './Cards/NationalReds.png';
-cards[12] = './Cards/peanuts.jpg';
+cards[12] = './Cards/peanuts.png';
 cards[13] = './Cards/SWBlue.png';
 cards[14] = './Cards/SWRed.png';
 cards[15] = './Cards/SWWhite.png';
